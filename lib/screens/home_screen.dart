4b3349a997ac510e6a7f80c85b3ba8a59ac0c752
hasanhammoudah <https://github.com/hasanhammoudah/Dynamic-Chart-in-PHP>
2023-12-31 22:34:26 +0300

    NEW UPDATE

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:zoom_clone/utils/colors.dart';
+import 'package:zoom_clone/widgets/home_meeting_button.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,8 +10,96 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int _page = 0;
+  onPageChanged(int page) {
+    setState(() {
+      _page = page;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: backgroundColor,
+        //elevation: 0,
+        title: const Text('Meet & Chat'),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              HomeMeetingButton(
+                onTap: () {},
+                icon: Icons.videocam,
+                text: 'New Meeting',
+              ),
+              HomeMeetingButton(
+                onTap: () {},
+                icon: Icons.add_box_rounded,
+                text: 'Join Meeting',
+              ),
+              HomeMeetingButton(
+                onTap: () {},
+                icon: Icons.calendar_today,
+                text: 'Schedule',
+              ),
+              HomeMeetingButton(
+                onTap: () {},
+                icon: Icons.arrow_upward_rounded,
+                text: 'Share Screen',
+              ),
+            ],
+          ),
+          const Expanded(
+            child: Center(
+              child: Text(
+                'Create/Join Meetings with just a click!',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          selectedItemColor: Colors.white,
+          backgroundColor: footerColor,
+          unselectedItemColor: Colors.grey,
+          onTap: onPageChanged,
+          currentIndex: _page,
+          type: BottomNavigationBarType.fixed,
+          unselectedFontSize: 14,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.comment_bank,
+              ),
+              label: 'Meet & Char',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.lock_clock,
+              ),
+              label: 'Meetings',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.person_outline,
+              ),
+              label: 'Contacts',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.settings_outlined,
+              ),
+              label: 'Settings',
+            ),
+          ]),
+    );
   }
 }
