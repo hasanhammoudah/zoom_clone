60b73a298dde295c6bb82e9a2d587bc85ef0909c
hasanhammoudah <https://github.com/hasanhammoudah/Dynamic-Chart-in-PHP>
2023-12-31 13:47:17 +0300

    NEW UPDATE

---
+++ lib/widgets/custom_button.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:zoom_clone/utils/colors.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key, required this.text, this.onPressed});
+  final String text;
+  final void Function()? onPressed;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(18.0),
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: buttonColor,
+          minimumSize: const Size(double.infinity, 50),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30),
+            side: const BorderSide(color: buttonColor),
+          ),
+        ),
+        child: Text(
+          text,
+          style: const TextStyle(fontSize: 17),
+        ),
+      ),
+    );
+  }
+}
